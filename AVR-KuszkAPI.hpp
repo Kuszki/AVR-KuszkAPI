5493477296ac06ddb6293ba68dd308d59c777d06
Łukasz Dróżdż <l.drozdz@o2.pl>
2014-12-10 21:50:23 +0100

    Add docs

---
+++ AVR-KuszkAPI.hpp
@@ -0,0 +1,39 @@
+/*! \file AVR-KuszkAPI.hpp
+ *  \brief Plik pomocniczy.
+ *
+ * Plik służący jedynie w celu wygenerowania głównej strony dokumentacji.
+ *
+ */
+
+/*! \mainpage AVR KuszkAPI
+
+    \section basic_sec Wstęp
+
+    Projekt jest zbiorem klas ułatwiających programowanie mikrokontrolerów w architekturze AVR z rodziny Arduino.
+
+    Jeśli znalazłeś błąd, masz jakieś propozycje lub uwagi, zgłoś to w <a target="_blank" href="https://github.com/Kuszki/AVR-KuszkAPI/issues">bugtrackerze</a> na GitHubie.
+
+    \section license_sec Licencja
+
+    Niniejszy program jest wolnym oprogramowaniem; możesz go rozprowadzać dalej i/lub modyfikować na warunkach Powszechnej Licencji Publicznej GNU, wydanej przez Fundację Wolnego Oprogramowania - według wersji 2-giej tej Licencji lub którejś z późniejszych wersji.
+
+    Niniejszy program rozpowszechniany jest z nadzieją, iż będzie on użyteczny - jednak BEZ JAKIEJKOLWIEK GWARANCJI, nawet domyślnej gwarancji PRZYDATNOŚCI HANDLOWEJ albo PRZYDATNOŚCI DO OKREŚLONYCH ZASTOSOWAŃ. W celu uzyskania bliższych informacji - Powszechna Licencja Publiczna GNU.
+
+    Z pewnością wraz z niniejszym programem otrzymałeś też egzemplarz Powszechnej Licencji Publicznej GNU (GNU General Public License); jeśli nie - napisz do Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+    \section install_sec Instalacja
+
+    Aby używać projektu należy rozpakować go do katalogu bibliotek uzytkownika programu Arduino i dołączyć do projektu dyrektywą `#inclide <nazwa_klasy.h>`, lub wybrać w IDE opcję "dodaj bibliotekę".
+
+    \section links_sec Przydatne linki
+
+    <ul>
+          <li>Projekt w serwisie GitHub: https://github.com/Kuszki/AVR-KuszkAPI</li>
+          <li>Strona autora: http://kuszki.myftp.org</li>
+          <li>Mail autora: l.drozdz@o2.pl</li>
+          <li>Treść licencji (angielski): http://www.gnu.org/licenses/gpl-2.0.html</li>
+          <li>Treść licencji (polski): http://gnu.org.pl/text/licencja-gnu.html</li>
+    </ul>
+
+*/
