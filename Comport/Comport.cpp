46fcb2fa517f6df4405ccdb3e1d9f0fbd98cb4bc
Kuszki <l.drozdz@o2.pl>
2015-03-04 23:07:55 +0100

    Implement own comport support

---
+++ Comport/Comport.cpp
@@ -0,0 +1,132 @@
+#include "Comport.h"
+
+volatile Comport::Buffer bRecv = {0, 0};
+volatile Comport::Buffer bSend = {0, 0};
+
+volatile Comport::Buffer* Comport::bIN	= &bRecv;
+volatile Comport::Buffer* Comport::bOUT	= &bSend;
+
+Comport::Comport(unsigned uRate)
+{
+	uRate = (F_CPU / 8 / uRate - 1) / 2;
+
+	UCSR0A = 0;
+	UCSR0C = 0x06;
+
+	UBRR0H = uRate >> 8;
+	UBRR0L = uRate;
+}
+
+Comport::~Comport(void)
+{
+	Stop();
+
+	UCSR0A = 0;
+	UCSR0C = 0;
+
+	UBRR0H = 0;
+	UBRR0L = 0;
+}
+
+void Comport::Start(void)
+{
+	UCSR0B = 152;
+}
+
+void Comport::Stop(void)
+{
+	while (bOUT->uHead != bOUT->uTail);
+
+	UCSR0B = 0;
+
+	bIN->uHead = bIN->uTail;
+}
+
+void Comport::Send(char cChar)
+{
+	register unsigned short uHead = (bOUT->uHead + 1) & (BUFF_SIZE - 1);
+
+    while (uHead == bOUT->uTail) {}
+
+    bOUT->pcBuff[uHead] = cChar;
+    bOUT->uHead = uHead;
+
+    UCSR0B |= (1 << UDRE0);
+}
+
+void Comport::Send(const char* pcData)
+{
+	register char cBuff;
+
+	while (cBuff = *pcData++) Send(cBuff);
+}
+
+void Comport::Send(const void* pvData, size_t sBytes)
+{
+	register const char* pcData = (char*) pvData;
+
+	while (sBytes--) Send(*pcData++);
+}
+
+char Comport::Recv(void)
+{
+    if (bIN->uHead == bIN->uTail) return 0;
+
+    bIN->uTail = (bIN->uTail + 1) & (BUFF_SIZE - 1);
+
+    return bIN->pcBuff[bIN->uTail];
+}
+
+bool Comport::Recv(void* pvData, size_t sBytes)
+{
+	static size_t sPos = 0;
+
+	register char* ptData = (char*) pvData;
+
+	while (bIN->uHead != bIN->uTail)
+	{
+		bIN->uTail = (bIN->uTail + 1) & (BUFF_SIZE - 1);
+
+		ptData[sPos++] = bIN->pcBuff[bIN->uTail];
+
+		if (sPos == sBytes)
+		{
+			sPos = 0;
+
+			return true;
+		}
+	}
+
+	return false;
+}
+
+bool Comport::Ready(void) const
+{
+	return (bIN->uHead != bIN->uTail);
+}
+
+ISR(USART_RX_vect)
+{
+    register char cBuff = UDR0;
+
+    register unsigned short uHead = (bRecv.uHead + 1) & (BUFF_SIZE - 1);
+
+    if (uHead != bRecv.uTail)
+    {
+		bRecv.uHead = uHead;
+
+		bRecv.pcBuff[uHead] = cBuff;
+    }
+}
+
+ISR(USART_UDRE_vect)
+{
+    if (bSend.uHead != bSend.uTail)
+    {
+    	bSend.uTail = (bSend.uTail + 1) & (BUFF_SIZE - 1);
+
+    	UDR0 = bSend.pcBuff[bSend.uTail];
+    }
+    else UCSR0B &= ~(1 << UDRIE0);
+}
+
---
+++ Comport/Comport.h
@@ -0,0 +1,161 @@
+/***********************************************************************
+
+    Klasa Comport biblioteki AVR KuszkAPI v1.0
+    Copyright (C) 2014 Łukasz "Kuszki" Dróżdż
+
+    Niniejszy program jest wolnym oprogramowaniem; możesz go
+    rozprowadzać dalej i/lub modyfikować na warunkach Powszechnej
+    Licencji Publicznej GNU, wydanej przez Fundację Wolnego
+    Oprogramowania - według wersji 2-giej tej Licencji lub którejś
+    z późniejszych wersji.
+
+    Niniejszy program rozpowszechniany jest z nadzieją, iż będzie on
+    użyteczny - jednak BEZ JAKIEJKOLWIEK GWARANCJI, nawet domyślnej
+    gwarancji PRZYDATNOŚCI HANDLOWEJ albo PRZYDATNOŚCI DO OKREŚLONYCH
+    ZASTOSOWAŃ. W celu uzyskania bliższych informacji - Powszechna
+    Licencja Publiczna GNU.
+
+    Z pewnością wraz z niniejszym programem otrzymałeś też egzemplarz
+    Powszechnej Licencji Publicznej GNU (GNU General Public License);
+    jeśli nie - napisz do Free Software Foundation, Inc., 675 Mass Ave,
+    Cambridge, MA 02139, USA.
+
+***********************************************************************/
+
+/*! \file Comport.h
+ *  \brief Zawiera deklaracje klasy Switch.
+ *
+ * Deklaracja zamieszczona w innym pliku pomaga uniknąć chaosu w organizacji kodu.
+ *
+ *  \file Comport.cpp
+ *  \brief Zawiera implementacje klasy Switch.
+ *
+ * Implementacja każdej klasy w innym pliku pomaga uniknąć chaosu w organizacji kodu.
+ *
+ */
+
+#ifndef _AVR_KUSZKAPI_COMPORT_H
+#define _AVR_KUSZKAPI_COMPORT_H
+
+#include <Arduino.h>
+
+#define BUFF_SIZE 32
+
+/*! \brief Obsługa połączeń przychodzących i wychodzących portu szeregowego.
+ *  \note Ze względu na prostotę zrezygnowano z rozwiązania szablonowego.
+ *
+ * Klasa umożliwia wysyłanie i odbieranie danych za pośrednictwem interfejsu RS232.
+ *
+ */
+class Comport
+{
+
+	public:
+
+		/*! \brief Struktura bufora danych.
+		 *
+		 * Struktura ma na celu przechowywanie informacji o stanie bufora danych wejścia i wyjścia.
+		 *
+		 */
+		struct Buffer
+		{
+			char pcBuff[BUFF_SIZE];	//!< Bufor na dane.
+
+			unsigned short uHead;	//!< Koniec danych.
+			unsigned short uTail;	//!< Początek danych.
+		};
+
+	protected:
+
+		volatile static Buffer* bIN;	//!< Bufor danych wejściowych.
+		volatile static Buffer* bOUT;	//!< Bufor danych wyjściowych.
+
+	public:
+
+		/*! \brief Konstruktor obiektu.
+		 *  \param [in] uRate Określa prędkość transmisji danych.
+		 *
+		 * Inicjuje rejestry informujące o parametrach transmisji danych..
+		 *
+		 */
+		Comport(unsigned uRate = 9600);
+
+		/*! \brief Destruktor obiektu.
+		 *
+		 * Wyłącza interfejs i zeruje stan rejestrów.
+		 *
+		 */
+		~Comport(void);
+
+		/*! \brief Inicjuje transmisje danych.
+		 *
+		 * Ustawia wybrane rejestry tak, by umożliwić transmisje danych przez port szeregowy.
+		 *
+		 */
+		void Start(void);
+
+		/*! \brief Zatrzymuje działanie interfejsu.
+		 *
+		 * Czeka na wysłanie reszty danych i kończy działanie interfejsu.
+		 *
+		 */
+		void Stop(void);
+
+		/*! \brief Wysyła jeden bajt danych.
+		 *  \param [in] cChar Dane.
+		 *
+		 * Sprawdza czy bufor wejściowy nie jest przepełniony i wysyła wybrany bajt danych.
+		 *
+		 */
+		void Send(char cChar);
+
+		/*! \brief Wysyła ciąg znaków.
+		 *  \param [in] pcData Dane.
+		 *
+		 * Sprawdza czy bufor wejściowy nie jest przepełniony i wysyła dane na które wskazuje parametr funkcji aż do napotkania terminatora.
+		 *
+		 */
+		void Send(const char* pcData);
+
+		/*! \brief Wysyła dane binarne.
+		 *  \param [in] pvData	Dane.
+		 *  \param [in] sBytes	Rozmiar danych.
+		 *
+		 * Sprawdza czy bufor wejściowy nie jest przepełniony i wysyła podaną liczbę bajtów na które wskazuje parametr funkcji.
+		 *
+		 */
+		void Send(const void* pvData, size_t sBytes);
+
+		/*! \brief Odbiera dane binarne.
+		 *  \return Odebrane dane.
+		 *  \note Gdy bufor wejściowy jest pusty metoda zwróci 0.
+		 *  \see Ready().
+		 *
+		 * Odbiera jeden bajt danych.
+		 *
+		 */
+		char Recv(void);
+
+		/*! \brief Odbiera dane binarne.
+		 *  \param [out]	pvData	Dane.
+		 *  \param [in]	sBytes	Rozmiar danych.
+		 *  \return Stan operacji:\n
+		 *          false - nie pobrano wszystkich danych,\n
+		 *          true - pobrano wszystkie dane.
+		 *
+		 * Odbiera dane aż do zapełnienia bufora podają ilościa bajtów. Gdy bufor wejścia opróżni się przed pobraniem wszystkich danych metoda zwróci wartość fałsz. Gdy uda się zapełnić bufor metoda zwróci wartość prawda.
+		 *
+		 */
+		bool Recv(void* pvData, size_t sBytes);
+
+		/*! \brief Sprawdzenie czy istnieją w buforze oczekujące dane.
+		 *  \return Stan bufora wejścia.
+		 *
+		 * Jeśli w buforze wejścia oczekują dane do pobrania to funkcja zwróci wartość prawda, w przeciwnym razie zwróci fałsz.
+		 *
+		 */
+		bool Ready(void) const;
+
+};
+
+#endif
